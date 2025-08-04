6d053460a6ba2c4bf56e711ab7b698d43bdba6e2
Carlos563429 <carlitosfg.5051@gmail.com>
2025-08-03 18:43:32 -0600

    [⭐] Commit: minor changes
    Changed def name

--- src/GHUpdateChecker/mod/github_api.py
+++ src/GHUpdateChecker/mod/github_api.py
@@ -9,11 +9,11 @@ class GitHubAPI:
         self.repo = repo
         self.URL = f"https://api.github.com/repos/{self.username}/{self.repo}"
 
-    def get_latest_release(self):
+    def get_latest_release_tag(self):
         """Get the latest release information from the GitHub repository."""
         response = requests.get(f"{self.URL}/releases/latest")
         if response.status_code == 200:
-            return response.json()["0"]["tag_name"]
+            return response.json()["tag_name"]
         else:
             warnings.warn(f"Failed to retrieve latest release: {response.status_code}") 
             return None
